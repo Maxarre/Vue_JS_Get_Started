376f0a670d2e7490f487ac57ff5b6fe36e0fe238
Maxence Poncelet <maxenceponcelet@hotmail.com>
2020-11-29 15:53:56 +0100

    TWOTTER

---
+++ src/assets/users.js
@@ -0,0 +1,41 @@
+export const users = [
+    {
+        "id": 1,
+        "username": "MaxPonc",
+        "firstName": "Maxence",
+        "lastName": "Poncelet",
+        "email": "maxenceponcelet@maillambda.com",
+        "isAdmin": true,
+        "twoots": [
+            { "id": 1, "content": "Twotter is Amazing!" },
+            { "id": 2, "content": "Don't forget to subscriber to Yolo!" }
+        ]
+    },
+    {
+        "id": 2,
+        "username": "Maxarre",
+        "firstName": "Max",
+        "lastName": "Arre",
+        "email": null,
+        "isAdmin": false,
+        "twoots": []
+    },
+    {
+        "id": 3,
+        "username": "PPTP",
+        "firstName": "Petit",
+        "lastName": "Patapon",
+        "email": null,
+        "isAdmin": false,
+        "twoots": []
+    },
+    {
+        "id": 4,
+        "username": "MS",
+        "firstName": "Micro",
+        "lastName": "Soft",
+        "email": null,
+        "isAdmin": false,
+        "twoots": []
+    }
+  ];
